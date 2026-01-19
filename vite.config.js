0127ac8ca853b9ed1039f0065e369917adebc6a3
mpbozentka <mitchell.bozentka@gmail.com>
2026-01-19 06:15:58 -0600

    Mobile Usability

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import process from 'node:process'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  cacheDir: path.resolve(process.cwd(), 'node_modules/.vite'),
 })
